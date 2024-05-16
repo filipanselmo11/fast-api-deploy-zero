782c2d3c2eea0aca9da17a1dbd51fb276dea1010
Fílip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2024-05-16 13:27:27 -0400

    feat: add mais testes

---
+++ test/contas_a_pagar_e_receber/routers/test_integrado_contas_a_pagar_e_receber_router.py
@@ -0,0 +1,59 @@
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from main import app
+from sqlalchemy.orm import sessionmaker
+
+from shared.dependencies import get_db
+from shared.database import Base
+
+client = TestClient(app)
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+app.dependency_overrides[get_db] = override_get_db
+
+def test_dev_listar_contas_a_pagar_e_receber():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    client.post("/contas-a-pagar-e-receber", json={"desc":"Aluguel", "valor": 1000, "tipo":"pagar"})
+    response = client.get("/contas-a-pagar-e-receber")
+    assert response.status_code == 200
+    # assert response.json() == []
+
+
+def test_dev_criar_conta_a_pagar_e_receber():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    client.post("/contas-a-pagar-e-receber")
+    nova_conta = {
+        "desc": "Curso Python",
+        "valor": 123,
+        "tipo": "pagar",
+    }
+    nova_conta_copy = nova_conta.copy()
+    nova_conta_copy["id"] = 1
+    response = client.post("/contas-a-pagar-e-receber", json=nova_conta)
+    assert response.status_code == 201
+    assert response.json() == nova_conta_copy
+
+
+def test_dev_retornar_erro_excedido_descricao():
+    response = client.post("/contas-a-pagar-e-receber", json={
+        "desc": "Desc teste deve retornar",
+        "valor": 12321,
+        "tipo": "Pagar"
+    })
