e66bcd5b537f5be41ebfa0eba9e8257abd05e349
Fílip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2024-05-20 12:15:24 -0400

    feat: add tratativa de exceção

--- contas_a_pagar_e_receber/routers/contas_a_pagar_e_receber_router.py
+++ contas_a_pagar_e_receber/routers/contas_a_pagar_e_receber_router.py
@@ -1,11 +1,12 @@
 from decimal import Decimal
 from typing import List
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 
 from contas_a_pagar_e_receber.models.conta_a_pagar_receber_model import ContaPagarReceber
 from shared.dependencies import get_db
+from shared.exceptions import NotFound
 
 
 router = APIRouter(prefix="/contas-a-pagar-e-receber")
@@ -30,20 +31,20 @@ async def listar_contas(db: Session = Depends(get_db)) -> List[ContaPagarReceber
 
 @router.get("/{id_contar_pagar_receber}", response_model=ContaPagarReceberResponse, status_code=200)
 async def obter_uma_conta(id_conta_pagar_receber: int, db:Session=Depends(get_db)) -> ContaPagarReceberResponse:
-    conta_pagar_receber: ContaPagarReceber = db.query(ContaPagarReceber).get(id_conta_pagar_receber)
+    conta_pagar_receber = busca_conta_por_id(id_conta_pagar_receber, db)
     return conta_pagar_receber
 
 @router.post("", response_model=ContaPagarReceberResponse, status_code=201)
 async def criar_conta(conta_a_pagar_e_receber_request:ContaPagarReceberRequest, db: Session = Depends(get_db)) -> ContaPagarReceberResponse:
-    contas_a_pagar_e_receber = ContaPagarReceber(**conta_a_pagar_e_receber_request.model_dump())
-    db.add(contas_a_pagar_e_receber)
+    conta_pagar_receber = ContaPagarReceber(**conta_a_pagar_e_receber_request.model_dump())
+    db.add(conta_pagar_receber)
     db.commit()
-    db.refresh(contas_a_pagar_e_receber)
-    return contas_a_pagar_e_receber
+    db.refresh(conta_pagar_receber)
+    return conta_pagar_receber
 
 @router.put("/{id_contar_pagar_receber}", response_model=ContaPagarReceberResponse, status_code=200)
 async def atualizar_conta(id_conta_pagar_receber: int, conta_a_pagar_e_receber_request:ContaPagarReceberRequest, db: Session = Depends(get_db)) -> ContaPagarReceberResponse:
-    conta_pagar_receber: ContaPagarReceber = db.query(ContaPagarReceber).get(id_conta_pagar_receber)
+    conta_pagar_receber = busca_conta_por_id(id_conta_pagar_receber, db)
     conta_pagar_receber.tipo = conta_a_pagar_e_receber_request.tipo
     conta_pagar_receber.valor = conta_a_pagar_e_receber_request.valor
     conta_pagar_receber.desc = conta_a_pagar_e_receber_request.desc
@@ -54,7 +55,15 @@ async def atualizar_conta(id_conta_pagar_receber: int, conta_a_pagar_e_receber_r
 
 @router.delete("/{id_contar_pagar_receber}", status_code=204)
 async def deletar_conta(id_conta_pagar_receber: int, db: Session = Depends(get_db)) -> None:
-    conta_pagar_receber: ContaPagarReceber = db.query(ContaPagarReceber).get(id_conta_pagar_receber)
+    conta_pagar_receber = busca_conta_por_id(id_conta_pagar_receber, db)
     db.delete(conta_pagar_receber)
     db.commit()
     #return conta_pagar_receber
+
+
+
+def busca_conta_por_id(id: int, db: Session) -> ContaPagarReceber:
+    conta_pagar_receber = db.query(ContaPagarReceber).get(id)
+    if conta_pagar_receber is None:
+        raise NotFound("Conta a pagar e receber")
+    return conta_pagar_receber
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
-
 from contas_a_pagar_e_receber.routers import contas_a_pagar_e_receber_router
+from shared.exceptions import NotFound
+from shared.exceptions_handler import not_found_handler
+
 # from shared.database import Base, engine
 # from contas_a_pagar_e_receber.models.conta_a_pagar_receber_model import ContaPagarReceber
 
@@ -11,3 +13,7 @@ from contas_a_pagar_e_receber.routers import contas_a_pagar_e_receber_router
 app = FastAPI()
 
 app.include_router(contas_a_pagar_e_receber_router.router)
+app.add_exception_handler(NotFound, not_found_handler)
+
+
+
